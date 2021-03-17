061e4f172e44ab3e936a05401dcbaac116ca029f
Adil Özdurdu <aozdurdu@outlook.com>
2021-03-18 00:21:04 +0300

    19 Day

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,9 +14,12 @@ export class ProductComponent implements OnInit {
 
   products:Product[] = [];
   dataLoaded = false;
+  filterText="";
 
   constructor(private productService:ProductService, 
-    private activatedRoute:ActivatedRoute) { }
+    private activatedRoute:ActivatedRoute, 
+    private toastrService:ToastrService,
+    private cartService:CartService) { }
 
   ngOnInit(): void {
 
@@ -38,5 +43,9 @@ export class ProductComponent implements OnInit {
       this.products = response.data
       this.dataLoaded= true;
     })
+  }
+  addToCart(product:Product){
+    this.toastrService.success("Added to cart", product.productName)
+    this.cartService.addToCart(product);
   }
 }
